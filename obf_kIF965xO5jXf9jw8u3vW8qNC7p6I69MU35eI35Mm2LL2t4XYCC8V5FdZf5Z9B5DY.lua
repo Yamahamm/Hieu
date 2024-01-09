458b6f359fa8a5479364c5811d490eb87bd3c6cd
Yamahamm <156053216+Yamahamm@users.noreply.github.com>
2024-01-09 13:14:20 +0700

    Add files via upload

---
+++ obf_kIF965xO5jXf9jw8u3vW8qNC7p6I69MU35eI35Mm2LL2t4XYCC8V5FdZf5Z9B5DY.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "NONAMEhud" -- Put You Name Script Here
+_G.Logo = "blob:https://taoanhdep.com/dc041f57-7dd8-4e47-b480-020f075381e2" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
